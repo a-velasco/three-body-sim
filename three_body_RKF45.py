182eded52011891390c7ada19bd3473dbe6464dc
Andrea Velasco <avgomes42@gmail.com>
2018-12-19 11:52:43 +0100

    Removing WIP sections - animation and MGR conditions

--- three_body_RKF45.py
+++ three_body_RKF45.py
@@ -5,100 +5,40 @@ import sys
 
 # Utility function to check the system's center of mass
 def center_of_mass(masses, positions):
+
 	total_mass = sum([m for m in masses]) 
 
 	return (1./total_mass) * sum(masses[i]*positions[i] for i in range(0,len(masses)))
 
 # Function to return default initial conditions
-def get_initial_conditions(kind='mgr', a=1.):
-	if kind == 'pythagorean':
-		masses=[3.,4.,5.]
-		x0 = np.array([[1.,3.], [-2.,-1.], [1.,-1.]])
-		v0 = np.array([[0.,0.], [0.,0.], [0.,0.]])
+def get_initial_conditions(a=1.):
 
-	elif kind == 'mgr':
-		# Vertices of an equilateral triangle
-		masses = [1.,1.,1.]
-		x_m1 = np.array([0.,0.])
-		x_m2 = np.array([a,0.])
-		x_m3 = np.array([a/2., a*np.sqrt(3.)/2.])
-		
-		x0 = np.array([x_m1, x_m2, x_m3])
-
-		# Tangent vectors
-		c = np.array([a/2.,a *np.sqrt(3.)/4.])  # center of concentric triangle
-		m1c = c - x_m1						    # vec. from center to vertex 1
-		v_m1 = np.array([-m1c[1],m1c[0]])	    # flip components & one sign to get perpendicular vec
-		v_m1 = 1.05*(1./np.linalg.norm(v_m1)) * v_m1 # normalize vector to avoid drift
-
-		m2c = c - x_m2
-		v_m2 = np.array([-m2c[1],m2c[0]])
-		v_m2 = 1.05*(1./np.linalg.norm(v_m2)) * v_m2
-
-		m3c = c - x_m3
-		v_m3 = np.array([-m3c[1],m3c[0]])
-		v_m3 = 1.05*(1./np.linalg.norm(v_m3)) * v_m3
-
-		v0 = -1*np.array([v_m1, v_m2, v_m3])
-	else:
-		print 'invalid initial conditions parameters'
-		sys.exit()
+	masses=[3.,4.,5.]
+	x0 = np.array([[1.,3.], [-2.,-1.], [1.,-1.]])
+	v0 = np.array([[0.,0.], [0.,0.], [0.,0.]])
 
 	return x0, v0, masses
 
 # Plotting function
-def plot_trajectories(m1x, m1y, m2x, m2y, m3x, m3y, masses, animate=False):
-	if animate == True:
-		fig = plt.figure()
-		ax = plt.axes(xlim=(-5, 5), ylim=(-5,5))
-		#ax = plt.axes(xlim=(-0.005, 0.005), ylim=(-0.008,0.008))
-		line1, = ax.plot([], [], 'r-',lw=1)
-		line2, = ax.plot([], [], 'g-',lw=1)
-		line3, = ax.plot([], [], 'b-',lw=1)
+def plot_trajectories(m1x, m1y, m2x, m2y, m3x, m3y, masses):
 
-		planet1, = ax.plot([], [], 'ro', label='m1', markersize=masses[0])
-		planet2, = ax.plot([], [], 'go', label='m2', markersize=masses[1])
-		planet3, = ax.plot([], [], 'bo', label='m3', markersize=masses[2])
+	#m1 trajectory
+	plt.plot(m1x[0], m1y[0], 'rx', markersize=4, label='start')
+	plt.plot(m1x[len(m1x)-1], m1y[len(m1y)-1], 'ro', markersize=masses[0])
+	plt.plot(m1x, m1y, 'r-', label=r'$m_{1}$', lw=1)
+	#m2 trajectory
+	plt.plot(m2x[0], m2y[0], 'gx', markersize=4)
+	plt.plot(m2x[len(m2x)-1], m2y[len(m2y)-1], 'go', markersize=masses[1])
+	plt.plot(m2x, m2y, 'g-', label=r'$m_{2}$', lw=1)
+	#m3 trajectory
+	plt.plot(m3x[0], m3y[0], 'bx', markersize=4)
+	plt.plot(m3x[len(m3x)-1], m3y[len(m3y)-1], 'bo', markersize=masses[2])
+	plt.plot(m3x, m3y, 'b-', label=r'$m_{3}$', lw=1)
 
-		def init():
-			line1.set_data([], [])
-			line2.set_data([], [])
-			line3.set_data([], [])
-			planet1.set_data([], [])
-			planet2.set_data([], [])
-			planet3.set_data([], [])
-			return line1, line2, line3, planet1, planet2, planet3
-
-		def animate(i):
-			#print 'frame',i,'/',len(m1x)
-			line1.set_data(m1x[0:i],m1y[0:i])
-			planet1.set_data(m1x[i],m1y[i])
-			
-			line2.set_data(m2x[0:i],m2y[0:i])
-			planet2.set_data(m2x[i],m2y[i])
-			
-			line3.set_data(m3x[0:i],m3y[0:i])
-			planet3.set_data(m3x[i],m3y[i])
-			return line1, line2, line3, planet1, planet2, planet3
-
-		anim = animation.FuncAnimation(fig, animate, np.arange(1, len(m1x)), init_func=init,
-	                                   interval=50, blit=True)
-	else:
-		#m1 trajectory
-		plt.plot(m1x[0], m1y[0], 'rx', markersize=3)
-		plt.plot(m1x[len(m1x)-1], m1y[len(m1y)-1], 'ro', markersize=masses[0])
-		plt.plot(m1x, m1y, 'r-', label='m1', lw=1)
-		#m2 trajectory
-		plt.plot(m2x[0], m2y[0], 'gx', markersize=3)
-		plt.plot(m2x[len(m2x)-1], m2y[len(m2y)-1], 'go', markersize=masses[1])
-		plt.plot(m2x, m2y, 'g-', label='m2', lw=1)
-		#m3 trajectory
-		plt.plot(m3x[0], m3y[0], 'bx', markersize=3)
-		plt.plot(m3x[len(m3x)-1], m3y[len(m3y)-1], 'bo', markersize=masses[2])
-		plt.plot(m3x, m3y, 'b-', label='m3', lw=1)
 	plt.xlabel('x')
 	plt.ylabel('y')
-	plt.legend()		
+	plt.legend()
+
 	plt.show()
 
 # Given the masses and positions of 3 bodies at a given time, calculate acceleration of each planet
@@ -125,6 +65,7 @@ def accel(masses, x):
 
 # 4th and 5th order Runge-Kutta-Fehlberg algorithm
 def RKF45(x0, v0, masses, dt):
+
 	a  = np.array([[0.         , 0.           , 0.          , 0.		  , 0.		],
             	   [1./4.      , 0.           , 0.          , 0.		  , 0.   	],
                    [3./32.     , 9./32.       , 0.          , 0.		  , 0.   	],
@@ -163,7 +104,7 @@ def RKF45(x0, v0, masses, dt):
 	return x4, v4, x5, v5
 
 # Function that calls RKF45 scheme iteratively with an adapting timestep
-def solver(x0, v0, dt, t, T, masses, max_epsilon): 
+def solver(x0, v0, dt, t, T, masses, max_epsilon):
 
 	# initialize arrays to store all accepted 5th order solutions
 	x_array = x0
@@ -179,7 +120,6 @@ def solver(x0, v0, dt, t, T, masses, max_epsilon):
 	epsilon = 0.
 
 	while t < T:
-		print dt
 		x4_new, v4_new, x5_new, v5_new = RKF45(x, v, masses, dt)
 
 		# Check q and adapt dt in any case
@@ -233,4 +173,4 @@ if __name__ == '__main__':
 
 	#### Calculate and plot trajectory of three bodies from t to T
 	m1x_full, m1y_full, m2x_full, m2y_full, m3x_full, m3y_full, t_array = solver(x0, v0, dt, t, T, masses, max_epsilon)
-	plot_trajectories(m1x_full, m1y_full, m2x_full, m2y_full, m3x_full, m3y_full, masses, animate=True)
+	plot_trajectories(m1x_full, m1y_full, m2x_full, m2y_full, m3x_full, m3y_full, masses)
